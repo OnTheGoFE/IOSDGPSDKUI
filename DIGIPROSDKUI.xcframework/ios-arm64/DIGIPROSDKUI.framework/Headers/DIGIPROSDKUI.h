8269b7a08fbfe14198af66a09101e075e088e324
Jonathan Viloria M <jonathanv@digipro.com.mx>
2020-09-28 10:57:40 -0500

    Version 1.27.4.900

---
+++ DIGIPROSDKUI.xcframework/ios-arm64/DIGIPROSDKUI.framework/Headers/DIGIPROSDKUI.h
@@ -0,0 +1,19 @@
+//
+//  DIGIPROSDKUI.h
+//  DIGIPROSDKUI
+//
+//  Created by Jonathan Viloria M on 5/2/19.
+//  Copyright © 2019 Jonathan Viloria M. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for DIGIPROSDKUI.
+FOUNDATION_EXPORT double DIGIPROSDKUIVersionNumber;
+
+//! Project version string for DIGIPROSDKUI.
+FOUNDATION_EXPORT const unsigned char DIGIPROSDKUIVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DIGIPROSDKUI/PublicHeader.h>
+
+
